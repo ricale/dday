fadb81ed25ca32851c762a9c5da9503fc84f2b27
KIM Kangseong <kangseong.kim@willowstone.kr>
2020-01-13 02:44:04 +0900

    Update AddDdayActivity for set image

--- app/src/main/java/com/example/dday/AddDdayActivity.kt
+++ app/src/main/java/com/example/dday/AddDdayActivity.kt
@@ -3,6 +3,7 @@ package com.example.dday
 import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Rect
 import android.os.Bundle
 import android.text.Editable
@@ -11,12 +12,20 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dday.model.Dday
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputEditText
 import org.threeten.bp.LocalDate
+import java.io.FileNotFoundException
 
 class AddDdayActivity : AppCompatActivity() {
+    companion object {
+        const val REQUEST_GET_IMAGE = 101
+    }
+    private lateinit var imageView: ImageView
+    private lateinit var imageButton: FloatingActionButton
     private lateinit var nameEditText: TextInputEditText
     private lateinit var dateEditText: TextInputEditText
     private lateinit var okButton: Button
@@ -32,40 +41,32 @@ class AddDdayActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_dday)
 
+        imageView = findViewById(R.id.ddayBgImage)
+        imageButton = findViewById(R.id.ddayBgImageButton)
         nameEditText = findViewById(R.id.ddayName)
         dateEditText = findViewById(R.id.ddayDate)
         okButton = findViewById(R.id.ddayAddButton)
 
         initDefaultDate()
+        setEventHandlers()
+    }
 
-        nameEditText.addTextChangedListener(object: TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                name = s.toString()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQUEST_GET_IMAGE) {
+            if(resultCode == RESULT_OK) {
+                if(data != null) {
+                    try {
+                        val imageUri = data.data
+                        val imageStream = contentResolver.openInputStream(imageUri!!)
+                        val selectedImage = BitmapFactory.decodeStream(imageStream)
+                        imageView.setImageBitmap(selectedImage)
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    }
+                }
             }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-        })
-
-        dateEditText.setOnClickListener {
-            showCalendarDialog()
         }
-
-        okButton.setOnClickListener {
-            val newOne = Dday(
-                name,
-                getString(R.string.date_string, year, month, dayOfMonth)
-            )
-            newOne.save()
-
-            val returnIntent = Intent()
-            returnIntent.putExtra("index", newOne.index)
-            setResult(RESULT_OK, returnIntent)
-            finish()
-        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
@@ -107,6 +108,43 @@ class AddDdayActivity : AppCompatActivity() {
             today.monthValue,
             today.dayOfMonth
         )
+    }
+
+    private fun setEventHandlers() {
+        imageButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, REQUEST_GET_IMAGE);
+        }
+
+        nameEditText.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                name = s.toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+        })
+
+        dateEditText.setOnClickListener {
+            showCalendarDialog()
+        }
+
+        okButton.setOnClickListener {
+            val newOne = Dday(
+                name,
+                getString(R.string.date_string, year, month, dayOfMonth)
+            )
+            newOne.save()
+
+            val returnIntent = Intent()
+            returnIntent.putExtra("index", newOne.index)
+            setResult(RESULT_OK, returnIntent)
+            finish()
+        }
     }
 
     private fun showCalendarDialog() {
